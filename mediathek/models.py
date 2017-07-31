e70f496270f8c08143a593c50ef1ee25fe93f3e3
Johann Mähr <johann@jm-p.de>
2017-07-31 14:54:45 +0200

    delete unused attributes

--- mediathek/models.py
+++ mediathek/models.py
@@ -5,32 +5,19 @@ from fsmedhrocore.models import BasicHistory
 
 class Kunde(BasicHistory):
     """
-    Extends django.contrib.auth.models.User if already registered
+    Extends django.contrib.auth.models.User
     """
-    user = models.OneToOneField(User, blank=True, null=True)  # optional link to fsmedhro-User
+    user = models.OneToOneField(User)
 
     anschrift = models.CharField(max_length=255, blank=True, null=True)
-    bemerkung = models.TextField(blank=True, null=True)
     tel = models.CharField(max_length=16, blank=True, null=True)
-
-    # old information. Will be ignored if 'user' is set
-    kunden_id = models.PositiveIntegerField(blank=True, null=True)
-    kunden_name = models.CharField(max_length=30, blank=True, null=True)
-    kunden_vorname = models.CharField(max_length=30, blank=True, null=True)
-    kunden_email = models.CharField(max_length=30, blank=True, null=True)
-    # geschlecht_id_f = models.ForeignKey(Tblgeschlecht)
-    # studg_id_f = models.ForeignKey(Tblstudiengang)
-    # kunden_timestamp = models.DateTimeField()
+    bemerkung = models.TextField(blank=True, null=True)
 
     def __str__(self):
         """
         returns "[Vorname] [Nachname]"
         """
-        if self.user:
-            return self.user.get_full_name()
-        else:
-            full_name = '%s %s' % (self.kunden_vorname, self.kunden_name)
-            return full_name.strip()
+        return self.user.get_full_name()
 
     class Meta:
         verbose_name = "Kundin/Kunde"
