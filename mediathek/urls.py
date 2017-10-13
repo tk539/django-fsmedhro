e0a6576d910bfdf7ad643b4926ba1ffda46f566f
Johann Mähr <johann@jm-p.de>
2017-10-13 18:07:49 +0200

    basic site layout

--- mediathek/urls.py
+++ mediathek/urls.py
@@ -5,4 +5,5 @@ app_name = 'mediathek'
 urlpatterns = [
     url(r'^$', views.mediathek_index, name='mediathek_index'),
     url(r'^verwaltung/$', views.mediathek_verwaltung, name='mediathek_verwaltung'),
+    url(r'^verwaltung/ausleihe$', views.ausleihe, name='ausleihe'),
 ]
--- mediathek/views.py
+++ mediathek/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, user_passes_test
+from django.core.exceptions import ObjectDoesNotExist
+from mediathek.models import Kunde
 
 
 def check_mediathek_mitarbeiter(user):
@@ -8,10 +10,28 @@ def check_mediathek_mitarbeiter(user):
 
 @login_required
 def mediathek_index(request):
-    return render(request, 'mediathek/index.html')
+
+    user = request.user
+
+    # Kundendaten abfragen. Wenn noch kein Kundenprofil, dann neu anlegen
+    try:
+        kunde = user.kunde
+    except ObjectDoesNotExist:
+        kunde = Kunde(user=user, modified_by=user)
+        kunde.save()
+
+    context = {'kunde': kunde,}
+
+    return render(request, 'mediathek/index.html', context)
 
 
 @login_required
 @user_passes_test(check_mediathek_mitarbeiter, login_url='mediathek:mediathek_index', redirect_field_name=None)
 def mediathek_verwaltung(request):
     return render(request, 'mediathek/verwaltung.html')
+
+
+@login_required
+@user_passes_test(check_mediathek_mitarbeiter, login_url='mediathek:mediathek_index', redirect_field_name=None)
+def ausleihe(request):
+    return render(request, 'mediathek/ausleihe.html')
