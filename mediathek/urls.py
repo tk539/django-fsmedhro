566546ab081062b8bc51f56223aa7942b4688225
Johann Mähr <johann@jm-p.de>
2017-10-13 15:17:50 +0200

    views und templates angelegt

---
+++ mediathek/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'mediathek'
+urlpatterns = [
+    url(r'^$', views.mediathek_index, name='mediathek_index'),
+    url(r'^verwaltung/$', views.mediathek_verwaltung, name='mediathek_verwaltung'),
+]
--- mediathek/views.py
+++ mediathek/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, user_passes_test
 
-# Create your views here.
+
+def check_mediathek_mitarbeiter(user):
+    return user.groups.filter(name='mediathek').exists()
+
+
+@login_required
+def mediathek_index(request):
+    return render(request, 'mediathek/index.html')
+
+
+@login_required
+@user_passes_test(check_mediathek_mitarbeiter, login_url='mediathek:mediathek_index', redirect_field_name=None)
+def mediathek_verwaltung(request):
+    return render(request, 'mediathek/verwaltung.html')
