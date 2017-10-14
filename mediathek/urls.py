07a3d6f9a397c191f9ac25155098060901db7a35
Johann Mähr <johann@jm-p.de>
2017-10-14 21:27:12 +0200

    views und templates for Sammelbestellungen anlegen

--- mediathek/urls.py
+++ mediathek/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'mediathek'
 urlpatterns = [
-    url(r'^$', views.mediathek_index, name='mediathek_index'),
-    url(r'^verwaltung/$', views.mediathek_verwaltung, name='mediathek_verwaltung'),
-    url(r'^verwaltung/ausleihe$', views.ausleihe, name='ausleihe'),
+    url(r'^$', views.index, name='index'),
+    url(r'^verwaltung/$', views.verwaltung, name='mediathek_verwaltung'),
+    #url(r'^verwaltung/ausleihe$', views.ausleihe, name='ausleihe'),
 ]
--- mediathek/views.py
+++ mediathek/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.core.exceptions import ObjectDoesNotExist
-from mediathek.models import Kunde
+from mediathek.models import Kunde, Bestellung, Sammelbestellung, Ware, Angebot
 
 
 def check_mediathek_mitarbeiter(user):
@@ -9,7 +9,7 @@ def check_mediathek_mitarbeiter(user):
 
 
 @login_required
-def mediathek_index(request):
+def index(request):
 
     user = request.user
 
@@ -20,7 +20,7 @@ def mediathek_index(request):
         kunde = Kunde(user=user, modified_by=user)
         kunde.save()
 
-    context = {'kunde': kunde,}
+    context = {'kunde': kunde, }
 
     return render(request, 'mediathek/index.html', context)
 
@@ -28,10 +28,107 @@ def mediathek_index(request):
 @login_required
 @user_passes_test(check_mediathek_mitarbeiter, login_url='mediathek:mediathek_index', redirect_field_name=None)
 def mediathek_verwaltung(request):
+    """
+    Verwaltung-Übersicht
+    :param request:
+    :return:
+    """
     return render(request, 'mediathek/verwaltung.html')
 
 
 @login_required
-@user_passes_test(check_mediathek_mitarbeiter, login_url='mediathek:mediathek_index', redirect_field_name=None)
+@user_passes_test(check_mediathek_mitarbeiter, login_url='mediathek:index', redirect_field_name=None)
+def sammelbest_list(request):
+    return render(request, 'mediathek/sammelbest_list.html')
+
+
+@login_required
+@user_passes_test(check_mediathek_mitarbeiter, login_url='mediathek:index', redirect_field_name=None)
+def sammelbest_detail(request, sammelbest_id):
+    """
+    Sammelbestellung bearbeiten (Angebote hinzufügen, Frist setzen etc.)
+    :param request:
+    :param sammelbest_id:
+    :return:
+    """
+
+    sammelbest = get_object_or_404(Sammelbestellung, pk=sammelbest_id)
+
+    return render(request, 'mediathek/sammelbest_detail.html')
+
+
+@login_required
+@user_passes_test(check_mediathek_mitarbeiter, login_url='mediathek:index', redirect_field_name=None)
+def sammelbest_auftraege_list(request, sammelbest_id):
+    """
+    Alle Bestellungen zu einer Sammelbestellung
+    :param request:
+    :param sammelbest_id:
+    :return:
+    """
+
+    sammelbest = get_object_or_404(Sammelbestellung, pk=sammelbest_id)
+
+    return render(request, 'mediathek/sammelbest_auftraege_list.html')
+
+
+@login_required
+def sammelbest_auftrag_detail(request, auftrag_id):
+    """
+    Eine Bestellung aufgeben/ansehen/bearbeiten
+    :param request:
+    :param auftrag_id:
+    :return:
+    """
+    bestellung = get_object_or_404(Bestellung, pk=auftrag_id)
+    
+    context = {'mitarbeiter': check_mediathek_mitarbeiter(request.user), }
+       
+    return render(request, 'mediathek/sammelbest_auftrag_detail.html')
+
+
+@login_required
+@user_passes_test(check_mediathek_mitarbeiter, login_url='mediathek:index', redirect_field_name=None)
+def waren_list(request):
+    """
+    Alle Bestellungen zu einer Sammelbestellung
+    :param request:
+    :return:
+    """
+    return render(request, 'mediathek/waren_list.html')
+
+
+@login_required
+@user_passes_test(check_mediathek_mitarbeiter, login_url='mediathek:index', redirect_field_name=None)
+def ware_detail(request, ware_id):
+    """
+    Sammelbestellung bearbeiten (Angebote hinzufügen, Frist setzen etc.)
+    :param request:
+    :param ware_id:
+    :return:
+    """
+
+    ware = get_object_or_404(Ware, pk=ware_id)
+
+    return render(request, 'mediathek/ware_detail.html')
+
+
+@login_required
+@user_passes_test(check_mediathek_mitarbeiter, login_url='mediathek:index', redirect_field_name=None)
+def angebot_detail(request, angebot_id):
+    """
+    Sammelbestellung bearbeiten (Angebote hinzufügen, Frist setzen etc.)
+    :param request:
+    :param angebot_id:
+    :return:
+    """
+
+    angebot = get_object_or_404(Angebot, pk=angebot_id)
+
+    return render(request, 'mediathek/angebot_detail.html')
+
+
+@login_required
+@user_passes_test(check_mediathek_mitarbeiter, login_url='mediathek:index', redirect_field_name=None)
 def ausleihe(request):
     return render(request, 'mediathek/ausleihe.html')
