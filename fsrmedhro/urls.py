7e2ba775cf5e49a4f60d49acb058300bfbe143c4
Martin Darmüntzel <martin@trivialanalog.de>
2019-05-05 17:11:10 +0200

    Fügt Login und Logout hinzu.

--- fsrmedhro/urls.py
+++ fsrmedhro/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', include('cms.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
