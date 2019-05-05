4b2af0f218782292ccedf1e23db2c4ffa59665bf
Martin Darmüntzel <martin@trivialanalog.de>
2019-05-05 16:07:44 +0200

    Fügt ein paar wichtige Settings hinzu.

--- fsrmedhro/settings.py
+++ fsrmedhro/settings.py
@@ -67,6 +67,10 @@ INSTALLED_APPS = [
 
 SITE_ID = 1
 
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -154,6 +158,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_DIR, "static"),
+]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
